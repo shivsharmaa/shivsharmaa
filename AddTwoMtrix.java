d877b23cc9d7ba1febee7d60997455e1db1be1ea
Shivam Sharma <sharmaa.shiv54@gmail.com>
2023-06-19 18:14:38 +0530

    initial commit

---
+++ AddTwoMtrix.java
@@ -0,0 +1,42 @@
+package arraypkg;
+import java.util.*;
+public class AddTwoMtrix {
+	public static void main(String[] args) {
+
+		int a[][]={{1,3,4},{2,4,3},{3,4,5}};    
+		int b[][]={{1,3,4},{2,4,3},{1,2,4}};    
+		
+		int[][] c=new int[3][3];
+System.out.println("...... . A ..............");
+for(int i=0; i<3; i++)
+{
+	for(int j=0; j<3; j++)
+	{
+		System.out.print(a[i][j]+" ");
+	}
+	System.out.println();
+}
+
+System.out.println("...... B ........");
+for(int i=0; i<3; i++)
+{
+	for(int j=0; j<3; j++)
+	{
+		System.out.print(b[i][j]+" ");
+	}
+	System.out.println();
+}
+
+System.out.println("............................");
+		for(int i=0; i<3; i++)
+		{
+			for(int j=0; j<3; j++)
+			{
+				c[i][j]=a[i][j]*b[i][j];
+				System.out.print(c[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+}
